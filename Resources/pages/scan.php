4edb62268107edfb38d78ef670dc2e5906a533e6
Ian Hernandez <ihernandez248@gmail.com>
2019-03-15 16:10:57 -0400

    Add files via upload

---
+++ Resources/pages/scan.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <title>Barcode Reader App</title>
+    <script>
+    window.onload = function() {
+  document.getElementById("user").focus();
+};
+    </script>
+  </head>
+  <body>
+    <!-- Main Menu Buttons -->
+   <center>
+       <div id="cFormL">
+       <h2>Scan Mode:</h2>
+
+        <div id="btns">
+          <form action="scan.php" method="post">
+            <label for="name">Click box below and scan ID:</label>
+
+            <input onfocus="if(this.value == 'Scan Barcode') { this.value = ''; }"
+                    onblur="if (this.value == '') { this.value = 'Scan Barcode'; }"
+                   value= "Scan Barcode" type="text" class="input" name="user"  id="user">
+
+        <div id="btnsView">
+        <a href="../../index.html"><input type="button" class="btn" value="Back" name="submitButton"></a>
+				<input type="submit" value="Submit" class="btn" id="submit" name="submitButton">
+            </div>
+      </form>
+        </div>
+
+       </div></center>
+       <?php
+               $isClicked = isset($_POST['submitButton']);
+
+               if($isClicked){
+               try{
+                   // Pulls Form Data
+                   $user = $_POST["user"];
+                   $name = $_POST["name"];
+
+                   // Establishes Connection
+                   $conn = new PDO("mysql:host=localhost:3308;dbname=scanApp", "root", "root");
+                   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                   $dataReader == false;
+
+                   //Confirms assetTag is unique and executes Query if it is
+                   if($dataReader == false){
+                       $query = 'INSERT INTO entries(user) VALUES(:user)';
+                       $statement = $conn->prepare($query);
+                       $statement->bindValue(':user', $user);
+                       $statement->execute();
+                       }else{
+                           echo "<script type='text/javascript'>alert('$message');</script>";
+                   }
+               }
+
+                   // Final Connection Check
+                   catch(PDOException $e){
+                   echo $conn . "<br>ERROR:" . $e->getMessage();
+                   echo 'Exception -> ';
+                   var_dump($e->getMessage());
+                   }
+                 }
+
+           ?>
+    <!-- Footer -->
+    <footer>Tecnologik Webapp</footer>
+</body>
+
+</html>
---
+++ Resources/pages/view.php
@@ -0,0 +1,70 @@
+<?php
+$conn = new PDO("mysql:host=localhost:3308;dbname=scanApp", "root", "root");
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Barcode Reader App</title>
+  </head>
+  <body>
+
+    <div id="cFormView" class="table">
+            <!-- Table Buttons -->
+            <div id="btnsView">
+            <a href="../../index.html"><input type="button" class= "btn" name="edit" value="Back"></a>
+            <a href="scan.php"><input type="submit" class= "btn" name="scan" value="Scan"></a>
+            <input type="text" id="searchInput" onkeyup="myFunction()" placeholder="Search..."><br />
+            </div>
+
+            <!-- Table Data -->
+        <div class="wrap">
+
+        <!-- PHP creates table with parsed HTML and collects data from DB via query -->
+        <?php
+        echo "<div id=\"container\" ><table id=\"invTable\">
+        <thead>
+        <tr>
+        <th>Entry</th>
+        <th>User</th>
+        <th>Date/Time</th>
+        </tr></thead><tbody id='searchTable'>";
+
+        $sql = "SELECT * FROM entries";
+
+        foreach ($conn->query($sql) as $row) {
+                print "<tr>";
+                print "<td>" . $row['id'];
+                print "<td>" . $row['user'];
+                print "<td>" . $row['date'];
+                print "</tr>";
+                }
+
+        echo "</tbody></table></div>";
+            ?>
+
+        </div>
+<script>
+        // Searches Text Input
+        var $rows = $('#searchTable tr');
+        $('#searchInput').keyup(function() {
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+        $rows.show().filter(function() {
+            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+    });
+
+</script>
+
+    </div>
+
+    <!-- Footer -->
+    <footer>Tecnologik Webapp</footer>
+</body>
+</html>
